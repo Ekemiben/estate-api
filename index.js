fb7ce0e1db44d22f2447ca3752e9a6082b04acd1
Ekemiben <ekemiben.4@gmail.com>
2024-08-25 16:54:55 +0100

    HTML directory removed

--- index.js
+++ index.js
@@ -56,11 +56,12 @@ app.use("/server/listing/", listingRouter);
 
 
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+// app.use(express.static(path.join(__dirname, '/client/dist')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-})
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+// })
+
 
 // Middleware to handle error
 app.use((err,req, res, next)=>{
